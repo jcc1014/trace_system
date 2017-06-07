d0f3c07bb735e2d213f4285d54d3e01c912e27ff
jcc1014 <1416119268@qq.com>
2017-06-07 18:44:35 +0800

    去除无用的引用包

--- src/main/java/com/mall/controller/UploadController.java
+++ src/main/java/com/mall/controller/UploadController.java
@@ -6,7 +6,6 @@ import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
-import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -19,7 +18,6 @@ import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
-import com.trace.po.Video;
 import com.trace.util.DateUtils;
 
 @Controller

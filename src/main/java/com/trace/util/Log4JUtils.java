62ed99e50c3af8d39f862136bdc38947508f3e59
jingcc <1416119268@qq.com>
2017-11-14 10:00:53 +0800

    111

--- src/main/java/com/trace/util/Log4JUtils.java
+++ src/main/java/com/trace/util/Log4JUtils.java
@@ -11,8 +11,8 @@ public class Log4JUtils{
     public static Logger getLogger(){  
        if (null == logger){  
             //Java8 废弃了Reflection.getCallerClass()  
-           logger = Logger.getLogger(Reflection.getCallerClass().getName());  
-           logger.debug("类名"+Reflection.getCallerClass().getName());  
+           logger = Logger.getLogger(Reflection.getCallerClass(0).getName());  
+           logger.debug("类名"+Reflection.getCallerClass(0).getName());  
        }  
         return logger;  
     }  
